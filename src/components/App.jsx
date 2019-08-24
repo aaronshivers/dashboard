6ffef097be1fe2d050d66582665f284b72afd315
aaron shivers <aaorn@moonmail.org>
2019-08-24 14:28:40 -0500

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const App = () => (
+  <Container>
+    <h1 className="display-1 text-center">dashboard</h1>
+  </Container>
+)
+
+export default App
